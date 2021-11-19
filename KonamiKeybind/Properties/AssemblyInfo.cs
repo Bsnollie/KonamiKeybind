f91c8abfb06ad25b474910465bfaa56be7a1f1f5
roy soriano <rodrigo_skate9@hotmail.com>
2021-11-19 16:25:21 -0600

    - Fixed bug where ducks without an input profile would cause the game to crash - Added check to make only local ducks be able to konami - Fixed versioning

--- KonamiKeybind/Properties/AssemblyInfo.cs
+++ KonamiKeybind/Properties/AssemblyInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyTitle("|DGRED|Konami Keybind")]
 [assembly: AssemblyCompany("Bsnollie")]
 [assembly: AssemblyDescription("Adds a keybind for konami'ng! |DGRED|P|DGBLUE|O|DGGREEN|P!")]
-[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
